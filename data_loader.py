84d99646af44f8b6c395ec38174a52910af88a7d
momo <ahdbsgh2@naver.com>
2021-06-20 02:37:12 +0900

    study

--- data_loader.py
+++ data_loader.py
@@ -16,3 +16,12 @@ json_data = pd.DataFrame(json_data['document'][0]["utterance"],
 json_data= json_data.drop(['id', 'start', 'end','note'], axis=1)
 
 print(json_data)
+
+
+for text in json_data:
+    tmp = []
+    json_data[id] == json_data[id] 
+
+    tmp.append(json_data['form'], json_data['original_form'])
+
+    print(tmp)
