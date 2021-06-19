fcc4e4c29d9a541870ceff41324803e9182b5d5f
momo <ahdbsgh2@naver.com>
2021-06-19 15:00:20 +0900

    study

--- data_loader.py
+++ data_loader.py
@@ -5,10 +5,14 @@ import pandas as pd
 with open('data/SDRW2000000001.json') as json_file:
     json_data = json.load(json_file)
 
-print(json_data.keys())
+# print(json_data.keys())
 
-print(json_data['document'][0])
+# print(json_data['document'][0]['utterance'])
 
-# json_data = pd.DataFrame(json_data['prizes'][0]["laureates"], 
-#                         columns = ['id', 'firstname', 'surname', 'share', 'motivation'])
+json_data = pd.DataFrame(json_data['document'][0]["utterance"], 
+                         columns = ['id', 'form', 'original_form', 'speaker_id', 'start', 'end', 'note'])
 
+
+json_data= json_data.drop(['id', 'start', 'end','note'], axis=1)
+
+print(json_data)
